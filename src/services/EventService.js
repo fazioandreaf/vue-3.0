2687044480fc4d710765934f11b813e7809eded0
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-08-21 10:41:49 +0200

    services

---
+++ src/services/EventService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+ const apiClient=axios.create({
+        baseURL:'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+        withCredentials:false,
+        headers:{
+            Accept:'application/json',
+            'Content-Type':'application/json',
+        }
+ })
+//   esporto un metodo chiamato getEvents
+ export default {
+     getEvents(){
+         return apiClient.get('/events')
+     }
+ }
